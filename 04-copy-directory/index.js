fb31f8991679f7093eeac01e00334ce6b8523f43
myspecialspace <myspecialspace@users.noreply.github.com>
2022-11-07 15:41:54 +0300

    feat: solve 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const path = require('node:path');
+const fs = require('node:fs/promises');
 
+
+const copyDir = async () => {
+    const SRC_DIR_PATH = path.resolve(__dirname, 'files');
+    const DEST_DIR_PATH = path.resolve(__dirname, 'files-copy');
+
+    await fs.rm(DEST_DIR_PATH, { recursive: true, force: true });
+    await fs.mkdir(DEST_DIR_PATH, { recursive: true });
+    const readObjects = await fs.readdir(SRC_DIR_PATH, { withFileTypes: true });
+
+    for (const readObject of readObjects) {
+        if (readObject.isFile()) {
+            const srcFilePath = path.resolve(SRC_DIR_PATH, readObject.name);
+            const destFilePath = path.resolve(DEST_DIR_PATH, readObject.name);
+
+            await fs.copyFile(srcFilePath, destFilePath);
+        }
+    }
+};
+
+copyDir();
